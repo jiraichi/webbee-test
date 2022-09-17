5e048172d0265a9945dc2655fe11be6d24cf91f8
jiraichi <shanvaliyev@hotmail.com>
2022-09-18 02:44:15 +0800

    fix: getmenuitems failed one

--- src/menu-items/entities/menu-item.entity.ts
+++ src/menu-items/entities/menu-item.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class MenuItem {
